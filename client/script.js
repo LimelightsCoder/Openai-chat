73e33e52c11b240ad98112f98f294cddf7ae1ccb
LimelightsCoder <cory.komaguchi@gmail.com>
2023-03-28 14:45:08 -0700

    update server url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server
 
-  const response = await fetch('https://my-chat-bot.onrender.com/', {
+  const response = await fetch('https://my-chat-bot.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
